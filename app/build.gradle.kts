f9f4a0c539d611fbfd1fd6a187b52267dbe42287
Meriem Mahfoud <meriemmahfoud02@gmail.com>
2025-05-18 17:57:29 +0200

    dependecies problem fixation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ android {
 
 dependencies {
 
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +51,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
     implementation(libs.books)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/example/bookstoreapp/ui/product/screens/HomeScreen.kt
+++ app/src/main/java/com/example/bookstoreapp/ui/product/screens/HomeScreen.kt
@@ -1,7 +1,10 @@
 package com.example.bookstoreapp.ui.product.screens
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -9,7 +12,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.bookstoreapp.ui.product.ProductIntent
 import com.example.bookstoreapp.ui.product.ProductViewModel
@@ -17,8 +22,8 @@ import com.example.bookstoreapp.ui.product.component.ProductsList
 
 @Composable
 fun HomeScreen(
-    viewModel: ProductViewModel = viewModel(),
-    onBookClick: (String) -> Unit
+    onBookClick: (String) -> Unit,
+    viewModel: ProductViewModel = viewModel()
 ) {
     val state by viewModel.state.collectAsState()
 
@@ -26,16 +31,22 @@ fun HomeScreen(
         viewModel.handleIntent(ProductIntent.LoadBooks)
     }
 
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+        contentAlignment = Alignment.Center
     ) {
         when {
             state.isLoading -> CircularProgressIndicator()
-            state.error != null -> Text("Erreur: ${state.error}", color = Color.Red)
-            else -> ProductsList(books = state.books, onBookClick = onBookClick)
+            state.error != null -> Text(
+                text = "Erreur: ${state.error}",
+                color = Color.Red
+            )
+            else -> ProductsList(
+                books = state.books,
+                onBookClick = onBookClick
+            )
         }
     }
 }
